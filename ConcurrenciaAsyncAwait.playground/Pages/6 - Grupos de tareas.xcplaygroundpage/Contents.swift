8335e718a57da2ba42094829f44f253b5a69fe1e
Rubén <ruben.sr.apple@gmail.com>
2024-12-03 11:55:11 +0100

    Mediante Task Groups se puede descargar varios archivos de forma concurrente

--- ConcurrenciaAsyncAwait.playground/Pages/6 - Grupos de tareas.xcplaygroundpage/Contents.swift
+++ ConcurrenciaAsyncAwait.playground/Pages/6 - Grupos de tareas.xcplaygroundpage/Contents.swift
@@ -1,7 +1,54 @@
-//: [Previous](@previous)
+import SwiftUI
 
-import Foundation
+let jsonImages = URL(string: "https://acoding.academy/testData/testImages.json")!
 
-var greeting = "Hello, playground"
+struct Images: Codable {
+    let images: [URL]
+}
 
-//: [Next](@next)
+func getImagesSimple(url: URL) async throws -> UIImage? {
+    let (data, _) = try await URLSession.shared.data(from: url)
+    return UIImage(data: data)
+}
+
+/// Descarga la imagenes de forma serializada (una a una)
+func getImagesSerialize(jsonURL: URL) async throws -> [UIImage] {
+    var result: [UIImage] = []
+    let (data, _) = try await URLSession.shared.data(from: jsonURL)
+    let urls = try JSONDecoder().decode(Images.self, from: data).images
+    for url in urls {
+        if let imagen = try await getImagesSimple(url: url) {
+            result.append(imagen)
+        }
+    }
+    return result
+}
+
+/// Con esta función la descarga será de forma concurrente
+/// utilizando los Grupos de Tareas
+func getImagesConcurrent(jsonURL: URL) async throws -> [UIImage] {
+    var result: [UIImage] = []
+    
+    let (data, _) = try await URLSession.shared.data(from: jsonURL)
+    let urls = try JSONDecoder().decode(Images.self, from: data).images
+    
+    try await withThrowingTaskGroup(of: UIImage?.self) { group in
+        for url in urls {
+            group.addTask {
+                try await getImagesSimple(url: url)
+            }
+        }
+        
+        for try await image in group.compactMap({ $0 })  {
+            result.append(image)
+        }
+    }
+    
+    return result
+}
+
+Task {
+    let images = try await getImagesConcurrent(jsonURL: jsonImages)
+    images
+}
+
